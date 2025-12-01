0902129099772ab53dbde54f9fd3ed3507a4368e
Vanessa Susaya <susayavanessa9@gmail.com>
2025-11-30 20:35:50 -0500

    Primeros repositorios

---
+++ src/main/java/com/example/demo/repository/PrestamoRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.Prestamo;
+
+public interface PrestamoRepository extends JpaRepository<Prestamo, Long> {
+}
